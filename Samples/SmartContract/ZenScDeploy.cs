18e8badc52b2499fc8bffd18c3bd355cc2517bdf
Tomaž Vinko <tomaz@zenodys.com>
2018-03-19 15:51:55 +0100

    bytecode bugfix

--- Samples/SmartContract/ZenScDeploy.cs
+++ Samples/SmartContract/ZenScDeploy.cs
@@ -1,9 +1,10 @@
 using CommonInterfaces;
 using Nethereum.Geth;
-using Nethereum.RPC.Eth.DTOs;
+using Nethereum.Web3;
 using System;
 using System.Collections;
 using System.IO;
+using System.Linq;
 using System.Numerics;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -27,6 +28,7 @@ namespace ZenScDeploy
         string _defaultGas;
         string _providerUrl;
         int _unlockAccountDuration;
+        string _byteCode;
         ZenCsScriptData _scripts;
         object _syncCsScript = new object();
         #endregion
@@ -44,15 +46,15 @@ namespace ZenScDeploy
 
             // Sender address. ("0x9812db3e6c072a9731267485bd1ce075ae11e6a8")
             _senderAddress = element.GetElementProperty("SENDER_ADDRESS");
-            
+
             // Sender password. ("password")
             _password = element.GetElementProperty("SENDER_PASSWORD");
 
             // How much time will account be unlocked in seconds. (120)
             _unlockAccountDuration = Convert.ToInt32(element.GetElementProperty("UNLOCK_DURATION"));
-            
+
             // Default gas value. ("290000") 
-            _defaultGas = element.GetElementProperty("DEFAUT_GAS"); 
+            _defaultGas = element.GetElementProperty("DEFAUT_GAS");
 
             // Smart contract ABI ([{""constant"":false,""inputs"":[{""name"":""tvConsumption"",""type"":""int256""},
             //                      {""name"":""washingMachineConsumption"",""type"":""int256""}],""name"":""saveConsumptions"",""outputs"":[],
@@ -62,13 +64,29 @@ namespace ZenScDeploy
             //                      {""name"":""washingMachineConsumption"",""type"":""int256""}],""payable"":false,""stateMutability"":""nonpayable"",
             //                      ""type"":""constructor""}])
             _abi = element.GetElementProperty("ABI");
-            
+
+            // Smart contract bytecode ("0x6060604052341561000f57600080fd5b336000806101000a81548173ffffffffffffffffffffffffffffff
+            //                           ffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff1602179055506101e88061005e
+            //                           6000396000f30060606040526004361061004c576000357c0100000000000000000000000000000000000000
+            //                           000000000000000000900463ffffffff1680632e1a7d4d1461005157806341c0e1b514610074575b600080fd
+            //                           5b341561005c57600080fd5b6100726004808035906020019091905050610089565b005b341561007f576000
+            //                           80fd5b610087610127565b005b6000809054906101000a900473ffffffffffffffffffffffffffffffffffff
+            //                           ffff1673ffffffffffffffffffffffffffffffffffffffff163373ffffffffffffffffffffffffffffffffff
+            //                           ffffff161415156100e457600080fd5b3373ffffffffffffffffffffffffffffffffffffffff166108fc8290
+            //                           81150290604051600060405180830381858888f19350505050151561012457600080fd5b50565b6000809054
+            //                           906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffff
+            //                           ffffffffffff163373ffffffffffffffffffffffffffffffffffffffff1614151561018257600080fd5b6000
+            //                           809054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffff
+            //                           ffffffffffffffffff16ff00a165627a7a72305820fcfa5c13166ae4380eff32cd31a58f2e8f2b2dd4a72c37
+            //                           33bd9c6a33a1697fb70029")
+            _byteCode = element.GetElementProperty("BYTE_CODE");
+
             // Parse result tags (<result>Element Id</result>) defined by user and dynamically create assembly that will query element results.
             InitializeScript(elements, element);
         }
         #endregion
         #endregion
-        
+
         #region IZenAction Implementations
         #region Properties
         #region ID
@@ -102,8 +120,8 @@ namespace ZenScDeploy
         #region DeployContract
         async Task<string> DeployContract()
         {
-            var web3 = new Web3.Web3(_providerUrl);
-            web3.TransactionManager.DefaultGas =  BigInteger.Parse(_defaultGas);
+            var web3 = new Web3(_providerUrl);
+            web3.TransactionManager.DefaultGas = BigInteger.Parse(_defaultGas);
             web3.TransactionManager.DefaultGasPrice = Nethereum.Signer.Transaction.DEFAULT_GAS_PRICE;
 
             var unlockAccountResult = await web3.Personal.UnlockAccount.SendRequestAsync(_senderAddress, _password, _unlockAccountDuration);
@@ -126,11 +144,14 @@ namespace ZenScDeploy
         #region GetSmartContractArgs
         object[] GetSmartContractArgs()
         {
+            if (_scripts == null)
+                return null;
+
             object[] args = new object[_scripts.ScriptDoc.DocumentNode.Descendants("code").Count()];
             for (int i = 0; i < _scripts.ScriptDoc.DocumentNode.Descendants("code").Count(); i++)
                 args[i] = _scripts.ZenCsScript.RunCustomCode(_scripts.ScriptDoc.DocumentNode
                                                                               .Descendants("code").ElementAt(i).Attributes["id"].Value);
-            
+
             return args;
         }
         #endregion
@@ -139,6 +160,10 @@ namespace ZenScDeploy
         // Parse "result" tags and create assembly for dynamically getting results from elements, that are then input args to smart contract constructor
         void InitializeScript(Hashtable elements, IPlugin element)
         {
+            // TODO : Cache
+            if (string.IsNullOrEmpty(element.GetElementProperty("CONTRACT_CONSTRUCTOR_PARAMS")))
+                return;
+
             lock (_syncCsScript)
             {
                 if (_scripts == null)
@@ -147,7 +172,7 @@ namespace ZenScDeploy
                     foreach (string args in Regex.Split(element.GetElementProperty("CONTRACT_CONSTRUCTOR_PARAMS"), "#100#"))
                         sFunctions += ZenCsScriptCore.GetFunction("return " + elements + ";");
 
-                    _scripts = ZenCsScriptCore.Initialize(sFunctions, elements, element, 
+                    _scripts = ZenCsScriptCore.Initialize(sFunctions, elements, element,
                                Path.Combine("tmp", "SmartContractDeploy", element.ID + ".zen"), null, element.GetElementProperty("PRINT_CODE") == "1");
                 }
             }
